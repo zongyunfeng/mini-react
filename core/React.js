1e9e79223abdb2df8d10111a71e5d0a9de9048b2
yunfeng <zong8498@gmail.com>
2024-01-14 17:28:12 +0800

    feat:实现基本的调度

--- core/React.js
+++ core/React.js
@@ -13,16 +13,14 @@ function createElement(tag, props, ...children) {
   return {
     type: DomType.Element,
     tag: tag,
-    props: {
-      props: props,
-      children: children.map((child) => {
-        if (typeof child == "string") {
-          return createTextNode(child);
-        } else {
-          return child;
-        }
-      }),
-    },
+    props,
+    children: children.map((child) => {
+      if (typeof child == "string") {
+        return createTextNode(child);
+      } else {
+        return child;
+      }
+    }),
   };
 }
 
@@ -34,12 +32,12 @@ function render(el, container) {
 
   Object.keys(el.props).forEach((key) => {
     if (key !== "children") {
-      dom[key]= el.props[key];
+      dom[key] = el.props[key];
     }
   });
 
-  if (el.props.children) {
-    el.props.children.forEach((child) => {
+  if (el.children) {
+    el.children.forEach((child) => {
       render(child, dom);
     });
   }
@@ -47,9 +45,82 @@ function render(el, container) {
   container.append(dom);
 }
 
+let nextUnitOfWork = null;
+
+function renderV2(vDom, container) {
+  nextUnitOfWork = {
+    type: DomType.Element,
+    tag: "div",
+    dom: container,
+    children: [vDom],
+  };
+}
+
+function createDom(vDom) {
+  const dom =
+    vDom.type == DomType.Text
+      ? document.createTextNode(vDom.props.nodeValue)
+      : document.createElement(vDom.tag);
+  return dom;
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
+    // if (key !== "children") {
+    dom[key] = props[key];
+    // }
+  });
+}
+
+function workLoop(deadline) {
+  while (nextUnitOfWork && deadline.timeRemaining() > 0) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+  requestIdleCallback(workLoop);
+}
+
+function initChildren(fiber) {
+  const children = fiber.children || [];
+
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      dom: null,
+      type: child.type,
+      tag: child.tag,
+      parent: fiber,
+      sibling: null,
+      props: child.props,
+      child: null,
+      children: child.children,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    const dom = createDom(fiber);
+    fiber.dom = dom;
+    updateProps(dom, fiber.props);
+    fiber.parent.dom.append(dom);
+  }
+
+  initChildren(fiber);
+
+  return fiber.child || fiber.sibling || fiber.parent?.sibling;
+}
+
+requestIdleCallback(workLoop);
+
 const React = {
   createElement,
-  render,
+  render: renderV2,
 };
 
 export default React;
